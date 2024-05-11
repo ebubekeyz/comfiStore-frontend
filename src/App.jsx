24bbfd6dd8c08607410e75ea211803469b6941ca
ebubekeyz <ebubeofforjoe@gmail.com>
2024-05-11 21:13:55 +0100

    106

--- src/App.jsx
+++ src/App.jsx
@@ -179,7 +179,6 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
